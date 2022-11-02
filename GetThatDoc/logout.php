6205f05d50196ef97dfab83e22a663668a6f237b
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2022-11-02 12:42:19 +0530

    Update logout.php

--- GetThatDoc/logout.php
+++ GetThatDoc/logout.php
@@ -10,6 +10,10 @@ unset($_SESSION["userId"]);
 session_unset();
 session_destroy();
 }
+if (isset($_COOKIE["user"]) AND isset($_COOKIE["pass"])){
+    setcookie("user", '', time() - (3600));
+    setcookie("pass", '', time() - (3600));
+}
 echo '<script>confirm("Press Ok to Log Out.");
 window.location.href="http://localhost/GetThatDoc/home.php"; 
 </script>';
